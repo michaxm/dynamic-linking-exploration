682730af304d3f49e665792cb81f4104cd5ab97f
Axel Mannhardt <axel.mannhardt@freiheit.com>
2015-12-16 14:44:56 +0100

    added ghc direct dynamic linking - not working yet

--- link-dynamically.bash
+++ link-dynamically.bash
@@ -7,3 +7,7 @@ ghc -outputdir build -dynamic app/Main.hs -o dynlink-example -lctest -L./testlib
 echo "hs lib"
 ghc -outputdir build -dynamic app/Main.hs -o dynlink-example -lHStestlib1 -L./testlibs/testlib1/ -optl-Wl,-rpath,/./testlibs/testlib1/
 ./dynlink-example hs
+
+
+#TODO native ghc
+#ghc -shared -dynamic -fPIC -outputdir build -o dynlink-example-ghc app/GHCRuntimeLinking.hs testlibs/testlib1/libHStesthslib1-1.0-ghc7.8.4.so -lHStestlibhs1 -L./testlibs/testlib1/ -optl-Wl,-rpath,./testlibs/testlib1/ -v 
---
+++ testlibs/testlib1/src/HsLib.hs
@@ -0,0 +1,7 @@
+module HsLib where
+
+ctest3 :: Int -> Int
+ctest3 = (+2)
+
+someFunc :: String -> String
+someFunc = (++ " appended")
